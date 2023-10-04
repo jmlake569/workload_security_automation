8f3ed2bf153686377f93fbbfada022e3b9057753
Jacob Lake <jacob_lake@trendmicro.com>
2023-10-04 12:35:13 -0400

    add better error handling

--- agent_status/agent_status.py
+++ agent_status/agent_status.py
@@ -5,40 +5,58 @@ import csv
 REGION = "<YOUR-CLOUD-ONE-REGION-HERE>"
 C1_API_KEY = "<YOUR-API-KEY-HERE>"
 
+#API endpoint URL and headers
 c1 = f"https://workload.{REGION}.cloudone.trendmicro.com/api/computers"
 c1_headers = {"api-version": "v1", "Content-Type": "application/json", "Authorization": C1_API_KEY}
 
-response = requests.get(c1, headers=c1_headers)
-data = json.loads(response.content)
-
-#extracted data
-computers = data["computers"]
-
-#define CSV file name
-csv_filename = "computer_data.csv"
-
-#write extracted data to CSV
-with open(csv_filename, mode="w", newline="", encoding="utf-8") as csv_file:
-    fieldnames = ["HostName", "DisplayName", "Description", "LastIPUsed", "Platform", "AgentStatusMessages"]
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+try:
+    #make the API request
+    response = requests.get(c1, headers=c1_headers)
+    response.raise_for_status()  # Raise an exception if the response status code is not 2xx
     
-    writer.writeheader()
+    #parse the JSON response
+    data = json.loads(response.content)
     
-    for computer in computers:
-        host_name = computer["hostName"]
-        display_name = computer["displayName"]
-        description = computer["description"]
-        last_ip_used = computer["lastIPUsed"]
-        platform = computer["platform"]
-        agent_status_messages = ", ".join(computer["computerStatus"]["agentStatusMessages"])
+    if "computers" in data:
+        computers = data["computers"]
+    else:
+        raise KeyError("The 'computers' key was not found in the response.")
+    
+    #define CSV file name
+    csv_filename = "computer_data.csv"
+    
+    #write extracted data to CSV
+    with open(csv_filename, mode="w", newline="", encoding="utf-8") as csv_file:
+        fieldnames = ["HostName", "DisplayName", "Description", "LastIPUsed", "Platform", "AgentStatusMessages"]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
         
-        writer.writerow({
-            "HostName": host_name,
-            "DisplayName": display_name,
-            "Description": description,
-            "LastIPUsed": last_ip_used,
-            "Platform": platform,
-            "AgentStatusMessages": agent_status_messages
-        })
-
-print(f"Data has been written to '{csv_filename}'")
+        for computer in computers:
+            host_name = computer.get("hostName", "")
+            display_name = computer.get("displayName", "")
+            description = computer.get("description", "")
+            last_ip_used = computer.get("lastIPUsed", "")
+            platform = computer.get("platform", "")
+            
+            agent_status_messages = computer["computerStatus"].get("agentStatusMessages", [])
+            agent_status_messages = ", ".join(agent_status_messages)
+            
+            writer.writerow({
+                "HostName": host_name,
+                "DisplayName": display_name,
+                "Description": description,
+                "LastIPUsed": last_ip_used,
+                "Platform": platform,
+                "AgentStatusMessages": agent_status_messages
+            })
+    
+    print("Data has been successfully written to the CSV file.")
+    
+except requests.exceptions.RequestException as e:
+    print(f"Request to the API failed: {e}")
+except json.JSONDecodeError as e:
+    print(f"Failed to parse JSON response: {e}")
+except KeyError as e:
+    print(f"Key error: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
